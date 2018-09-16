e57480ffdb641020b4677c78d15247d6d7d6d084
Umair Qaisar <qaisaru@icloud.com>
2018-09-16 13:30:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -26,7 +26,7 @@ def reformat_languages(languages)
         end
         # this would cause us problems if we had more than one attribute we're iterating over
         language_attributes[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
-        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key                                
+        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key
         if language_attributes[language][attribute].nil?
           #if :ruby language key doesn't have a :type key nested within it
           language_attributes[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
